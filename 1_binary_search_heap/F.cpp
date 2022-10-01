d58b1e58adca5b255c4a6d0866689a640ee58978
Kokindos <kokindoss@gmail.com>
2022-10-01 20:10:53 +0300

    add solvings

--- 1_binary_search_heap/F.cpp
+++ 1_binary_search_heap/F.cpp
@@ -1,4 +1,22 @@
-//
-// Created by admin on 29.09.2022.
-//
+#include <iostream>;
 
+using namespace std;
+
+int main() {
+    unsigned long long n, a, b, w, h;
+    cin >> n >> a >> b >> w >> h;
+    unsigned long long left = 0;
+    unsigned long long right = w + h;
+    while (right - left > 1) {
+        unsigned long long middle = (left + right) / 2;
+        unsigned long long w0 = w / (a + 2 * middle);
+        unsigned long long w1 = w / (b + 2 * middle);
+        unsigned long long h0 = h / (b + 2 * middle);
+        unsigned long long h1 = h / (a + 2 * middle); //для разных ориентаций
+        if (w0 * h0 >= n || w1 * h1 >= n) {
+            left = middle;
+        } else
+            right = middle;
+    }
+    cout << left;
+}
--- 1_binary_search_heap/H.cpp
+++ 1_binary_search_heap/H.cpp
@@ -1,4 +1,93 @@
-//
-// Created by admin on 29.09.2022.
-//
+#include <vector>
+#include <iostream>
 
+using namespace std;
+
+struct Heap {
+    vector<int> tree;
+
+    void clear() {
+        tree.clear();
+    }
+
+    bool empty() {
+        return tree.empty();
+    }
+
+    int parent(int i) {
+        return (i - 1) / 2;
+    }
+
+    int left(int i) {
+        return (2 * i + 1);
+    }
+
+    int right(int i) {
+        return (2 * i + 2);
+    }
+
+    void siftUp(int i) {
+        while (i > 0 && tree[i] < tree[ (i - 1) / 2]) {
+            swap(tree[i], tree[ (i - 1) / 2]);
+            i = (i - 1) / 2;
+        }
+    }
+
+    void add(int value) {
+        tree.push_back(value);
+        siftUp(tree.size() - 1);
+    }
+
+    void siftDown(int i) {
+        while (true) {
+            if (i * 2 + 2 < tree.size()) {
+                if (tree[i] < tree[i * 2 + 1] && tree[i] < tree[i * 2 + 2])
+                    break;
+                else {
+                    int minChild = i * 2 + 1 < i * 2 + 2 ? i * 2 + 1 : i * 2 + 2;
+                    swap(tree[i], tree[minChild]);
+                    i = minChild;
+                }
+            }
+            else if (i * 2 + 1 < tree.size()) {
+                if (tree[i] > tree[i * 2 + 1]) {
+                    swap(tree[i], tree[i * 2 + 1]);
+                    i = i * 2 + 1;
+                } else
+                    break;
+            }
+            else
+                break;
+        }
+    }
+
+    int getDeleteMin() {
+        int ans = tree[0];
+        tree[0] = tree.back();
+        tree.pop_back();
+        siftDown(0);
+        return ans;
+    }
+};
+
+int main() {
+    Heap heap;
+    string op;
+    while (cin >> op) {
+        if (op == "ADD") {
+            int x;
+            cin >> x;
+            heap.add(x);
+        }
+        else if (op == "EXTRACT") {
+            if (heap.empty()) {
+                cout << "CANNOT\n";
+            } else {
+                cout << heap.getDeleteMin() << "\n";
+            }
+        }
+        else if (op == "CLEAR") {
+            heap.clear();
+        }
+    }
+}
